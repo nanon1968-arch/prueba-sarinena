fa4d472ab510b79dbf4d60453ddd65a5fd767fbd
nanon1968-arch <nanon1968@gmail.com>
2026-08-09 18:04:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // Los datos (tareas, usuarios, fotos) siempre se piden en vivo a Supabase,
 // así que hace falta conexión a internet para usar la app con normalidad.
 
-var CACHE_NAME = 'brigada-sarinena-v30';
+var CACHE_NAME = 'brigada-sarinena-v32';
 var ARCHIVOS_CACHE = [
   './',
   './index.html',
